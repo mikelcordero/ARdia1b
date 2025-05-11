8911262516aa9facc83ea0eb3f7c2965a7da30d0
Mikel <mikel.cordero@euneiz.com>
2025-05-11 12:00:07 +0200

    cambio boton volver

--- Assets/MikelCorderoMedina/Scripts/volverescena/VolverEscena0.cs
+++ Assets/MikelCorderoMedina/Scripts/volverescena/VolverEscena0.cs
@@ -1,18 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class VolverEscena0 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void VolverAlMenu()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(0); // Asegúrate de que Escena 0 esté en el Build Settings
     }
 }
